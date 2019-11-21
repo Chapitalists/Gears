b8f1e8275c2bbbbca9ff557fe746640071116700
cbossut <bossut.clement@gmail.com>
2019-11-21 02:01:51 +0100

    upgrade Link to draw Motor links and Algebraic links

--- src/Link.elm
+++ src/Link.elm
@@ -15,24 +15,62 @@ type alias Link =
     ( Id Gear, Id Gear )
 
 
-view : Coll Gear -> Link -> List (Svg msg)
-view gears l =
+viewFractLink : Coll Gear -> Link -> List (Svg msg)
+viewFractLink gears l =
     let
         getGear id =
             Coll.get id gears
     in
     case Tuple.mapBoth getGear getGear l of
         ( Just g, Just gg ) ->
-            [ drawLink ( Gear.getPos g, Gear.getPos gg ) <|
-                max (Gear.getLength g gears) (Gear.getLength gg gears)
+            [ drawRawLink ( Gear.getPos g, Gear.getPos gg ) <|
+                (Gear.getLength g gears + Gear.getLength gg gears)
+                    / 2
             ]
 
         _ ->
-            Debug.log "Didn’t found both gears to draw link" []
+            Debug.log "Didn’t found both gears to draw algebraic link" []
 
 
-drawLink : ( Vec2, Vec2 ) -> Float -> Svg msg
-drawLink ( p1, p2 ) gearL =
+viewMotorLink : Coll Gear -> Link -> List (Svg msg)
+viewMotorLink gears l =
+    let
+        getGear id =
+            Coll.get id gears
+    in
+    case Tuple.mapBoth getGear getGear l of
+        ( Just g, Just gg ) ->
+            [ drawMotorLink
+                ( ( Gear.getPos g, Gear.getLength g gears )
+                , ( Gear.getPos gg, Gear.getLength gg gears )
+                )
+            ]
+
+        _ ->
+            Debug.log "Didn’t found both gears to draw motor link" []
+
+
+drawMotorLink : ( ( Vec2, Float ), ( Vec2, Float ) ) -> Svg msg
+drawMotorLink ( ( p1, d1 ), ( p2, d2 ) ) =
+    let
+        dir =
+            Vec.direction p2 p1
+
+        contactPoint center diameter clockWise =
+            Vec.add center <|
+                Vec.scale (diameter / 2) (rotate90 dir clockWise)
+
+        gearL =
+            d1 + d2 / 2
+    in
+    S.g []
+        [ drawRawLink ( contactPoint p1 d1 True, contactPoint p2 d2 True ) gearL
+        , drawRawLink ( contactPoint p1 d1 False, contactPoint p2 d2 False ) gearL
+        ]
+
+
+drawRawLink : ( Vec2, Vec2 ) -> Float -> Svg msg
+drawRawLink ( p1, p2 ) gearL =
     S.polyline
         [ SA.points [ tupleFromVec p1, tupleFromVec p2 ]
         , SA.stroke <| Color.brown
@@ -42,42 +80,15 @@ drawLink ( p1, p2 ) gearL =
         []
 
 
-drawRefLink : ( Gear, Gear ) -> Coll Gear -> List (Svg msg)
-drawRefLink ( g, gg ) coll =
-    let
-        ( minG, maxG ) =
-            if (Fract.toFloat <| Gear.getFract g) < (Fract.toFloat <| Gear.getFract gg) then
-                ( g, gg )
-
-            else
-                ( gg, g )
-
-        centers =
-            ( Gear.getPos minG, Gear.getPos maxG )
-
-        dir =
-            Vec.direction (Tuple.second centers) (Tuple.first centers)
-
-        r =
-            Gear.getLength minG coll / 2
-
-        w =
-            r / 15
-
-        exC =
-            Vec.scale r <| vec2 -(Vec.getY dir) (Vec.getX dir)
-
-        d1 =
-            Vec.add exC
-
-        d2 =
-            Vec.add <| Vec.negate exC
-    in
-    [ drawLink (Tuple.mapBoth d1 d1 centers) w
-    , drawLink (Tuple.mapBoth d2 d2 centers) w
-    ]
-
-
 tupleFromVec : Vec2 -> ( Float, Float )
 tupleFromVec v =
     ( Vec.getX v, Vec.getY v )
+
+
+rotate90 : Vec2 -> Bool -> Vec2
+rotate90 v clockWise =
+    if clockWise then
+        vec2 (Vec.getY v) -(Vec.getX v)
+
+    else
+        vec2 -(Vec.getY v) (Vec.getX v)
